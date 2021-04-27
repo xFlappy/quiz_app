8b1cf60a603a4377d2675b130b8a224c237631d1
xFlappy <nic.degiorgi@gmail.com>
2021-04-27 14:44:37 +0200

    Hello app

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,10 @@ void main() {
   runApp(MyApp());
 }
 
+//void main() => runApp(MyApp()); //si può scrivere anche così, più semplice
+
 class MyApp extends StatelessWidget {
+  @override //fa il codice più pulito e chiaro, stiamo facendo un override della classe
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Text("Hello!"),
