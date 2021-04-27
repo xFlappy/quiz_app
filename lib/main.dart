e74a6e426d7b93c7abb8f9430e1a80a166906f9d
xFlappy <nic.degiorgi@gmail.com>
2021-04-27 14:42:24 +0200

    Hello app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Text("Hello!"),
+    );
   }
 }
