b6306e3b30e2370c4e3b84cfc9fb290ad9d48757
xFlappy <nic.degiorgi@gmail.com>
2021-04-27 17:53:26 +0200

    Styled question

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -10,18 +12,19 @@ class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  // la _ crea un privato
+  var _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
-      questionIndex++;
+      _questionIndex++;
     });
-    print(questionIndex);
+    print(_questionIndex);
   }
 
   @override //fa il codice più pulito e chiaro, stiamo facendo un override della classe
@@ -37,16 +40,23 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(questions[questionIndex]),
+            Question(
+              questions[_questionIndex],
+            ),
             RaisedButton(
-                child: Text("Answer 1"),
-                onPressed: () => print("Answer 1 chosen")),
+              child: Text("Answer 1"),
+              onPressed: () => print("Answer 1 chosen"),
+            ),
             RaisedButton(
-                child: Text("Answer 2"),
-                onPressed: () {
-                  print("Answer 2 chosen");
-                }),
-            RaisedButton(child: Text("Answer 3"), onPressed: answerQuestion),
+              child: Text("Answer 2"),
+              onPressed: () {
+                print("Answer 2 chosen");
+              },
+            ),
+            RaisedButton(
+              child: Text("Answer 3"),
+              onPressed: _answerQuestion,
+            ),
           ],
         ),
       ),
