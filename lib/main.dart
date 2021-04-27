af5ca568fab3ae82d4a4b53d3362998d9ba75931
xFlappy <nic.degiorgi@gmail.com>
2021-04-27 15:11:52 +0200

    Added column widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,24 @@ void main() {
 class MyApp extends StatelessWidget {
   @override //fa il codice più pulito e chiaro, stiamo facendo un override della classe
   Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite color?",
+      "What's your favorite animal?",
+    ];
     return MaterialApp(
-      home: Text("Hello!"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Column(
+          children: [
+            Text("The question!"),
+            RaisedButton(child: Text("Ansewer 1"), onPressed: null),
+            RaisedButton(child: Text("Ansewer 2"), onPressed: null),
+            RaisedButton(child: Text("Ansewer 3"), onPressed: null),
+          ],
+        ),
+      ),
     );
   }
 }
