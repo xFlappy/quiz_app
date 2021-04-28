7e7a0ee7292853adca4728c204930c47f7ae16ce
xFlappy <nic.degiorgi@gmail.com>
2021-04-28 08:30:17 +0200

    New answer widget created

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './question.dart';
+import 'answer.dart';
 
 void main() {
   runApp(MyApp());
@@ -30,8 +31,18 @@ class _MyAppState extends State<MyApp> {
   @override //fa il codice più pulito e chiaro, stiamo facendo un override della classe
   Widget build(BuildContext context) {
     var questions = [
-      "What's your favorite color?",
-      "What's your favorite animal?",
+      {
+        "questionText": "What's your favorite color?",
+        "answers": ["Black", "Red", "Green", "Blue"]
+      },
+      {
+        "questionText": "What's your favorite animal?",
+        "answers": ["Rabbit", "Snake", "Elefant", "Lion"]
+      },
+      {
+        "questionText": "Who is your favorite Teacher",
+        "answers": ["Max", "Max", "Max", "Max"]
+      },
     ];
     return MaterialApp(
       home: Scaffold(
@@ -41,22 +52,12 @@ class _MyAppState extends State<MyApp> {
         body: Column(
           children: [
             Question(
-              questions[_questionIndex],
+              questions[_questionIndex]["questionText"],
             ),
-            RaisedButton(
-              child: Text("Answer 1"),
-              onPressed: () => print("Answer 1 chosen"),
-            ),
-            RaisedButton(
-              child: Text("Answer 2"),
-              onPressed: () {
-                print("Answer 2 chosen");
-              },
-            ),
-            RaisedButton(
-              child: Text("Answer 3"),
-              onPressed: _answerQuestion,
-            ),
+            ...(questions[_questionIndex]["answers"] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
           ],
         ),
       ),
